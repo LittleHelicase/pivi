91adf9627fbfb53b723ee1320f637e9bfae6d280
Aron L. S <serial000001@starnitzke.eu>
2016-01-17 18:51:10 +0000

    Added Browser-Compability (HTML5 Canvas). Modified lib/commands/line_data.js to start lines with beginPath in order to ensure proper clearing of the canvas. Added Test-HTML. TODO: Update gulpfile

---
+++ piviBrowser.js
@@ -0,0 +1,65 @@
+/*
+  (c) 2015 Aron Lennart Starnitzke
+  based on PiVi by LittleHelicase
+
+  *** THIS IS A PROOF-OF-CONCEPT DESIGN. NO PRODUCTION USE! ***
+*/
+module.exports = function(cnvs, data) {
+  var frameLines;
+  var frameData;
+  var frameCurrent;
+  var frameTotal;
+  var docCtx;
+  var docCnvs;
+  var parser = require("./lib/grammar.js").parse;
+  var dtc = require("./lib/dataToCanvas.js");
+
+  initialize(cnvs, data); // prepare
+  draw();
+
+  function initialize(cnvs, data) {
+    // Initialize canvas
+    docCnvs = cnvs;
+    resetCanvas(docCnvs);
+
+    // Seperate each frame into an array
+    var frames = data.split("newframe");
+    frameCurrent = 0;
+    frameTotal = frames.length;
+
+    // Initialize the data arrays
+    frameLines = new Array(frames.length);
+    frameData = new Array(frames.length);
+    // Split the data line by line and parse it
+    for (var i = 0; i < frames.length; i++) {
+      frameLines[i] = frames[i].replace("\r", "").split("\n").filter(function(arg) {return arg.length > 0;});
+      frameData[i] = new Array(frameLines[i].length);
+      for (var b = 0; b < frameLines[i].length; b++) {
+        frameData[i][b] = parser(frameLines[i][b]);
+      }
+    }
+  }
+  function resetCanvas(cnvs) {
+    docCtx = cnvs.getContext("2d");
+    docCtx.fillStyle = "#000000";
+    docCtx.clearRect(0, 0, docCnvs.width, docCnvs.height);
+  }
+  function draw() {
+    // clear frame
+    docCtx.clearRect(0, 0, docCnvs.width, docCnvs.height);
+
+    // draw frame
+    for (var i = 0; i < frameData[frameCurrent].length; i++) {
+      dtc(docCtx, frameData[frameCurrent][i]);
+    }
+
+    // Increase frame
+    if (frameCurrent + 1 >= frameTotal)
+      frameCurrent = 0;
+    else
+      frameCurrent++;
+
+    if (frameTotal > 1)
+      requestAnimationFrame(draw);
+  }
+}
