99db681d7dabff2088c3575885afe9d7c61e0bf7
Aron L. S <serial000001@starnitzke.eu>
2016-01-24 13:31:38 +0000

    Gulpfile updated. Directory structure redefined

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 
 var gulp = require("gulp");
-
+var source = require('vinyl-source-stream');
+var browserify = require("browserify");
 var jshint = require("gulp-jshint");
 var handlebars = require("gulp-compile-handlebars");
 var rename = require("gulp-rename");
@@ -8,6 +9,32 @@ var peg = require("gulp-peg");
 var gutil = require("gulp-util");
 var mocha = require("gulp-mocha");
 var clean = require("gulp-clean");
+
+// All dynamically loaded libraries and their aliases.
+// One could do this a bit more elegant, however, this is the quick-and-dirty solution.
+var libs = [
+  ["./lib/commands/ellipse_data.js", "./commands/ellipse_data.js"],
+  ["./lib/commands/line_data.js", "./commands/line_data.js"],
+  ["./lib/commands/point_data.js", "./commands/point_data.js"],
+  ["./lib/commands/properties.js", "./commands/properties.js"],
+  ["./lib/commands/stack_operations.js", "./commands/stack_operations.js"]
+];
+
+gulp.task("build-browser", function() {
+  var b = browserify('./piviBrowser.js', { standalone: 'pivi' });
+
+  for (var i = 0; i < libs.length; i++) {
+    gutil.log('Inserting ' + libs[i][0] + ' as ' + libs[i][1] + '...');
+    b.require(libs[i][0], { expose: libs[i][1] });
+  }
+
+  return b.bundle()
+    .on('error', function (e) {
+      gutil.log(e);
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./browser'));
+});
 
 gulp.task("jshint", function(){
   gulp.src(["./lib/*.js","!./lib/grammar.js"])
@@ -18,7 +45,7 @@ gulp.task("jshint", function(){
 gulp.task("clean", function(){
   gulp.src("./lib/grammar.js")
     .pipe(clean());
-})
+});
 
 gulp.task("build-grammar", function(){
   gulp.src("./lib/grammar.peg")
@@ -49,11 +76,11 @@ gulp.task("build-specification", ["build-grammar"], function(){
     .pipe(handlebars({},options))
     .pipe(rename("specification.md"))
     .pipe(gulp.dest("./specification/"));
-})
+});
 
 gulp.task("run-tests", ["build-grammar"], function(){
   gulp.src("./test/*.js")
     .pipe(mocha());
-})
+});
 
-gulp.task("test", ["build-grammar", "run-tests"]);
+gulp.task("test", ["build-grammar", "build-browser", "run-tests"]);
